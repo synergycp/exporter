c28a1702fd89210b1493df2a788ebb4eedde243a
Zane Hooper <zanehoop@gmail.com>
2018-10-19 18:51:39 -0600

    Include API URL in config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const request = require('request-promise');
 const q = require('q');
-const {API_KEY, filters} = require(__dirname + '/../config.json');
+const {API_KEY, API_URL, filters} = require(__dirname + '/../config.json');
 
 request
   .get({
-    url: `https://api.cp.usdedicated.com/server`,
+    url: `${API_URL}/server`,
     qs: Object.assign({
       key: API_KEY,
     }, filters),
@@ -12,7 +12,7 @@ request
   .then(result => {
     return q.all(JSON.parse(result).data.data.map(server => {
       return request
-        .get(`https://api.cp.usdedicated.com/server/${server.id}/control?key=${API_KEY}`)
+        .get(`${API_URL}/server/${server.id}/control?key=${API_KEY}`)
         .then(result => {
           JSON.parse(result).data.data.map(control => {
             console.log([control.server.srv_id, control.ip, control.admin_password].join(','));
